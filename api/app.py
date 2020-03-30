d239ccef872f6211b2582042900689341491a7f8
Brandon Atkinson <tehremo@gmail.com>
2020-03-29 21:09:24 -0400

    Cleaning up some more code

--- api/app.py
+++ api/app.py
@@ -3,26 +3,24 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-
-
 socketio.init_app(app, cors_allowed_origins="*")
-
-
-@socketio.on('broadcast', namespace='/csirl')
-def test_message(message):
-    print(f'Broadcast: {message}')
-    emit('my response', {'data': message['data']}, broadcast=True)
 
 
 @socketio.on('connect', namespace='/csirl')
 def test_connect():
     print('Client connected.')
-    emit('my response', {'data': 'Connected'})
+    emit('response', {'data': 'Connected'})
 
 
 @socketio.on('disconnect', namespace='/csirl')
 def test_disconnect():
     print('Client disconnected.')
+
+
+@socketio.on('broadcast', namespace='/csirl')
+def test_message(message):
+    print(f'Broadcast: {message}')
+    emit('response', {'data': message['data']}, broadcast=True)
 
 
 if __name__ == '__main__':
